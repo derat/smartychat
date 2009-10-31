1de4c89510d4c930ed3bf658e50bedf05d853f51
Daniel Erat <dan@erat.org>
2009-10-31 12:39:21 -0700

    clean up tests and add subscription test

--- smartychat_test.rb
+++ smartychat_test.rb
@@ -63,6 +63,8 @@ class MockClient
 end
 
 class MockRoster
+  attr_reader :accepted_subscriptions
+
   def initialize
     @subscription_request_callback = nil
     @accepted_subscriptions = []
@@ -73,94 +75,103 @@ class MockRoster
   end
 
   def accept_subscription(from)
-    @accepted_subscriptions << from
+    @accepted_subscriptions << from.to_s
+  end
+
+  def deliver_subscription_request(from)
+    return if not @subscription_request_callback
+    presence = Jabber::Presence.new
+    presence.type = :subscribe
+    presence.from = from
+    @subscription_request_callback.call(nil, presence)
   end
 end
 
 class TestSmartychat < Test::Unit::TestCase
-  # Test a simple chat session.
-  def test_basic
-    client = MockClient.new
-    roster = MockRoster.new
-    chat = SmartyChat.new(
-      client, roster,
+  def setup
+    @client = MockClient.new
+    @roster = MockRoster.new
+    @chat = SmartyChat.new(
+      @client, @roster,
       { :logger => Logger.new(LOG_FILE),
         :message_buffer_sec => 0,
       })
-    assert_equal(nil, client.shift_presence.type)
+    assert_equal(nil, @client.shift_presence.type)
+  end
 
+  # Test that the server accepts subscription requests.
+  def test_subscribe
+    jid = 'foo@example.com'
+    @roster.deliver_subscription_request(jid)
+    assert_equal([jid], @roster.accepted_subscriptions)
+  end
+
+  # Test a simple chat session.
+  def test_basic
     jid1 = 'foo@example.com'
-    client.deliver_message(jid1, '/join #nerds')
-    chat.wait_until_all_messages_sent
-    assert_equal('_Created "#nerds"._', client.shift_message(jid1).body)
+    @client.deliver_message(jid1, '/join #nerds')
+    @chat.wait_until_all_messages_sent
+    assert_equal('_Created "#nerds"._', @client.shift_message(jid1).body)
     assert_equal('_Joined "#nerds" with 1 user total._',
-                 client.shift_message(jid1).body)
-    assert_equal(nil, client.shift_message(jid1))
+                 @client.shift_message(jid1).body)
+    assert_equal(nil, @client.shift_message(jid1))
 
     jid2 = 'bar@example.com'
-    client.deliver_message(jid2, '/join #nerds')
-    chat.wait_until_all_messages_sent
+    @client.deliver_message(jid2, '/join #nerds')
+    @chat.wait_until_all_messages_sent
     assert_equal('_*bar* <bar@example.com> has joined "#nerds"._',
-                 client.shift_message(jid1).body)
-    assert_equal(nil, client.shift_message(jid1))
+                 @client.shift_message(jid1).body)
+    assert_equal(nil, @client.shift_message(jid1))
     assert_equal('_Joined "#nerds" with 2 users total._',
-                 client.shift_message(jid2).body)
-    assert_equal(nil, client.shift_message(jid2))
+                 @client.shift_message(jid2).body)
+    assert_equal(nil, @client.shift_message(jid2))
 
-    client.deliver_message(jid1, 'hi bar!')
-    chat.wait_until_all_messages_sent
-    assert_equal(nil, client.shift_message(jid1))
-    assert_equal('*foo*: hi bar!', client.shift_message(jid2).body)
-    assert_equal(nil, client.shift_message(jid2))
+    @client.deliver_message(jid1, 'hi bar!')
+    @chat.wait_until_all_messages_sent
+    assert_equal(nil, @client.shift_message(jid1))
+    assert_equal('*foo*: hi bar!', @client.shift_message(jid2).body)
+    assert_equal(nil, @client.shift_message(jid2))
 
-    client.deliver_message(jid2, 'howdy')
-    chat.wait_until_all_messages_sent
-    assert_equal('*bar*: howdy', client.shift_message(jid1).body)
-    assert_equal(nil, client.shift_message(jid1))
-    assert_equal(nil, client.shift_message(jid2))
+    @client.deliver_message(jid2, 'howdy')
+    @chat.wait_until_all_messages_sent
+    assert_equal('*bar*: howdy', @client.shift_message(jid1).body)
+    assert_equal(nil, @client.shift_message(jid1))
+    assert_equal(nil, @client.shift_message(jid2))
 
-    client.deliver_message(jid1, '/part')
-    chat.wait_until_all_messages_sent
-    assert_equal('_Left "#nerds"._', client.shift_message(jid1).body)
-    assert_equal(nil, client.shift_message(jid1))
+    @client.deliver_message(jid1, '/part')
+    @chat.wait_until_all_messages_sent
+    assert_equal('_Left "#nerds"._', @client.shift_message(jid1).body)
+    assert_equal(nil, @client.shift_message(jid1))
     assert_equal('_*foo* <foo@example.com> has left "#nerds"._',
-                 client.shift_message(jid2).body)
-    assert_equal(nil, client.shift_message(jid2))
+                 @client.shift_message(jid2).body)
+    assert_equal(nil, @client.shift_message(jid2))
   end
 
   # Test that password-protected channels work as expected.
   def test_password
-    client = MockClient.new
-    roster = MockRoster.new
-    chat = SmartyChat.new(
-      client, roster,
-      { :logger => Logger.new(LOG_FILE),
-        :message_buffer_sec => 0,
-      })
-
     jid1 = 'foo@example.com'
-    client.deliver_message(jid1, '/join #nerds password')
-    chat.wait_until_all_messages_sent
-    assert_equal('_Created "#nerds"._', client.shift_message(jid1).body)
+    @client.deliver_message(jid1, '/join #nerds password')
+    @chat.wait_until_all_messages_sent
+    assert_equal('_Created "#nerds"._', @client.shift_message(jid1).body)
     assert_equal('_Joined "#nerds" with 1 user total._',
-                 client.shift_message(jid1).body)
-    assert_equal(nil, client.shift_message(jid1))
+                 @client.shift_message(jid1).body)
+    assert_equal(nil, @client.shift_message(jid1))
 
     jid2 = 'bar@example.com'
-    client.deliver_message(jid2, '/join #nerds')
-    chat.wait_until_all_messages_sent
-    assert_equal(nil, client.shift_message(jid1))
+    @client.deliver_message(jid2, '/join #nerds')
+    @chat.wait_until_all_messages_sent
+    assert_equal(nil, @client.shift_message(jid1))
     assert_equal('_Incorrect or missing password for "#nerds"._',
-                 client.shift_message(jid2).body)
-    assert_equal(nil, client.shift_message(jid2))
+                 @client.shift_message(jid2).body)
+    assert_equal(nil, @client.shift_message(jid2))
 
-    client.deliver_message(jid2, '/join #nerds password')
-    chat.wait_until_all_messages_sent
+    @client.deliver_message(jid2, '/join #nerds password')
+    @chat.wait_until_all_messages_sent
     assert_equal('_*bar* <bar@example.com> has joined "#nerds"._',
-                 client.shift_message(jid1).body)
-    assert_equal(nil, client.shift_message(jid1))
+                 @client.shift_message(jid1).body)
+    assert_equal(nil, @client.shift_message(jid1))
     assert_equal('_Joined "#nerds" with 2 users total._',
-                 client.shift_message(jid2).body)
-    assert_equal(nil, client.shift_message(jid2))
+                 @client.shift_message(jid2).body)
+    assert_equal(nil, @client.shift_message(jid2))
   end
 end
